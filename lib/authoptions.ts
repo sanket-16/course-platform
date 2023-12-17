43a079066ad246abb427c02e0949d0430bd58252
sanket-16 <san162002@gmail.com>
2023-12-17 19:15:58 +0530

    feat: prisma, next-auth, next-themes, shadcn setup

---
+++ lib/authoptions.ts
@@ -0,0 +1,17 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: String(process.env.GOOGLE_CLIENT_ID),
+      clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
+    }),
+  ],
+};
